787715db86578de2591c6747ff9097810311b920
zhaojintian <729234283@qq.com>
2018-08-06 18:48:03 +0800

    优化

--- src/modules/Permissions/List/index.vue
+++ src/modules/Permissions/List/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <List :current="current" :columns="columns" :data="roles.roles.items" :total="roles.roles.total"
+    <List :current="current" :columns="columns" :data="permissions.permissions.items" :total="permissions.permissions.total"
           @on-change="handlePageChange">
       <ListHeader>
         <ListOperations>
@@ -27,23 +27,6 @@
             </Col>
           </Row>
         </Form-item>
-        <Form-item label="包含权限">
-          <Row>
-            <Col span="20">
-              <Checkbox
-                v-if="permissions.permissions.items.length"
-                v-for="(item, index) in permissions.permissions.items" :key="index"
-                v-model="formData.permissions[item.id]"
-                @on-change="data => { handlePermissionsChange(item.id)(data) }">
-                {{ item.name }}
-              </Checkbox>
-              <div v-else>
-                暂无权限数据，请先
-                <Button @click="$router.push('/company-app/roles/permissions/permissions/index')">添加</Button>
-              </div>
-            </Col>
-          </Row>
-        </Form-item>
       </Form>
       <div slot="footer">
         <Button type="text" size="large" @click="formModal = false">取消</Button>
@@ -62,7 +45,6 @@
   export default {
     name: 'list',
     async beforeRouteUpdate (to, from, next) {
-      this.roles.roles = {}
       this.permissions.permissions = {}
 
       this.routePrefix = helpers.getRoutePrefix(to.params)
@@ -73,14 +55,12 @@
       next()
     },
     async created () {
-      this.roles.roles = {}
       this.permissions.permissions = {}
 
       this.routePrefix = helpers.getRoutePrefix(this.$route.params)
       this.alias = this.$route.params.alias
 
-      await this.getPermissionItems()
-      await this.getItems()
+      this.getItems()
     },
     components: {
       List,
@@ -97,9 +77,6 @@
           name: '',
           description: ''
         },
-        formData: {
-          permissions: {}
-        },
         ruleValidate: {
           name: [
             {
@@ -112,6 +89,7 @@
             }
           ]
         },
+
         put: {
           id: 0
         },
@@ -123,19 +101,6 @@
           {
             title: '名称',
             key: 'name'
-          },
-          {
-            title: '包含权限',
-            width: 300,
-            render: (h, params) => {
-              const { items } = this.permissions.permissions
-              const permissions = params.row.permissions.split(',').reverse().map(id => {
-                const item = helpers.getItemById(items, id)
-                return item.name || ''
-              }).filter(item => item !== '').join(',')
-
-              return h('span', null, permissions)
-            }
           },
           {
             title: '描述',
@@ -177,14 +142,13 @@
       }
     },
     computed: mapState([
-      'roles',
       'permissions'
     ]),
     methods: {
       getItems (current = 1) {
         this.current = current
 
-        return this.$store.dispatch('getRoles', {
+        return this.$store.dispatch('getPermissions', {
           query: {
             offset: (current - 1) * consts.PAGE_SIZE,
             limit: consts.PAGE_SIZE,
@@ -192,16 +156,8 @@
           }
         })
       },
-      getPermissionItems () {
-        return this.$store.dispatch('getPermissions', {
-          query: {
-            offset: 0,
-            limit: 1000
-          }
-        })
-      },
       getDetails () {
-        return this.$store.dispatch('getRole', { id: this.put.id })
+        return this.$store.dispatch('getPermission', { id: this.put.id })
       },
       handlePageChange (current) {
         this.getItems(current)
@@ -209,7 +165,6 @@
       handlePost () {
         this.formModal = true
         this.put.id = 0
-        this.$set(this.formData, 'permissions', {})
         this.$refs.formValidate.resetFields()
       },
       handlePut (id) {
@@ -222,7 +177,7 @@
         this.del.id = id
       },
       async handleDelOk () {
-        await this.$store.dispatch('deleteRole', {
+        await this.$store.dispatch('deletePermission', {
           id: this.del.id
         })
         this.$Message.success('删除成功！')
@@ -233,31 +188,13 @@
       handleFormOk () {
         this.$refs.formValidate.validate(async valid => {
           if (valid) {
-            const action = this.put.id ? 'putRole' : 'postRole'
-            const permissions = (permissions => {
-              let ret = []
-
-              Object.keys(permissions).forEach(key => {
-                if (permissions[key]) {
-                  ret.push(key)
-                }
-
-                return ret
-              })
-
-              return ret.join(',')
-            })(this.formData.permissions)
-
-            if (!permissions) {
-              this.$Message.error('包含权限不能为空')
-              return
-            }
+            console.log(this.put.id)
+            const action = this.put.id ? 'putPermission' : 'postPermission'
 
             await this.$store.dispatch(action, {
               id: this.put.id,
               body: {
                 ...this.formValidate,
-                permissions,
                 alias: this.alias
               }
             })
@@ -269,25 +206,13 @@
             this.getItems()
           }
         })
-      },
-      handlePermissionsChange (val) {
-        return data => {
-          this.formData.permissions[val] = data
-        }
       }
     },
     watch: {
-      'roles.role': {
+      'permissions.permission': {
         handler (newVal) {
-          const { id, permissions, ...others } = newVal
-
+          const { id, ...others } = newVal
           this.formValidate = others
-
-          this.$set(this.formData, 'permissions', {})
-
-          permissions.split(',').forEach(key => {
-            this.$set(this.formData.permissions, key, true)
-          })
         }
       }
     }
