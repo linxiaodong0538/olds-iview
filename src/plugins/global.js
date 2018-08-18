5ead83362c6248325fc899aac09143897a4d2555
zhaojintian <729234283@qq.com>
2018-08-18 17:07:44 +0800

    优化

--- src/plugins/global.js
+++ src/plugins/global.js
@@ -3,13 +3,12 @@ import helpers from '../utils/helpers/base'
 import consts from '../utils/consts'
 
 export default {
-  install (Vue, options) {
-    Vue.prototype.$consts = consts
-
-    Vue.prototype.$helpers = helpers
+  install (Vue) {
+    Vue.prototype.consts = consts
+    Vue.prototype.helpers = helpers
+    Vue.prototype.time = time
 
     Vue.filter('time', val => time.getTime(val))
-
     Vue.filter('date', val => time.getDate(val))
   }
 }
