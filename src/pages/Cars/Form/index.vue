723050deeadb51777de0f1006e1e0ccea8bea946
zhaojintian <729234283@qq.com>
2018-10-11 14:37:45 +0800

    update

--- src/pages/Cars/Form/index.vue
+++ src/pages/Cars/Form/index.vue
@@ -1,40 +1,86 @@
 <template>
   <div>
     <div class="limit-width">
-      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
-        <Form-item label="车牌号" prop="num">
-          <Input v-model="formValidate.num" placeholder="请输入车牌号"></Input>
+      <Form
+        ref="formValidate"
+        :model="formValidate"
+        :rules="ruleValidate"
+        :label-width="100">
+        <Form-item
+          label="车牌号"
+          prop="num">
+          <Input
+            v-model="formValidate.num"
+            placeholder="请输入车牌号" />
         </Form-item>
-        <Form-item label="车主" prop="owner">
-          <Input v-model="formValidate.owner" placeholder="请输入车主"></Input>
+        <Form-item
+          label="车主"
+          prop="owner">
+          <Input
+            v-model="formValidate.owner"
+            placeholder="请输入车主" />
         </Form-item>
-        <Form-item label="图片" prop="picture">
+        <Form-item
+          label="图片"
+          prop="picture">
           <Uploader
             ref="uploader"
             :has-default-file="!!id"
             v-model="formValidate.picture"
-            @change="handleUploaderChange"
-          />
+            @change="handleUploaderChange" />
           （尺寸：1150x647）
         </Form-item>
-        <Form-item label="年检时间" prop="mot_time">
-          <DatePicker v-model="formValidate.mot_time" type="date" placeholder="请选择年检时间"
-                      style="width: 220px"></DatePicker>
+        <Form-item
+          label="年检时间"
+          prop="mot_time">
+          <DatePicker
+            :value="formValidate.mot_time"
+            type="date"
+            placeholder="请选择年检时间"
+            style="width: 220px"
+            @on-change="value => { handleDatePickerChange('mot_time', value) }" />
         </Form-item>
-        <Form-item label="保险到期时间" prop="insurance_time">
-          <DatePicker v-model="formValidate.insurance_time" type="date" placeholder="请选择保险到期时间"
-                      style="width: 220px"></DatePicker>
+        <Form-item
+          label="保险到期时间"
+          prop="insurance_time">
+          <DatePicker
+            :value="formValidate.insurance_time"
+            type="date"
+            placeholder="请选择保险到期时间"
+            style="width: 220px"
+            @on-change="value => { handleDatePickerChange('insurance_time', value) }" />
         </Form-item>
-        <Form-item label="购买日期" prop="buy_time">
-          <DatePicker v-model="formValidate.buy_time" type="date" placeholder="请选择购买日期"
-                      style="width: 220px"></DatePicker>
+        <Form-item
+          label="购买日期"
+          prop="buy_time">
+          <DatePicker
+            :value="formValidate.buy_time"
+            type="date"
+            placeholder="请选择购买日期"
+            style="width: 220px"
+            @on-change="value => { handleDatePickerChange('buy_time', value) }" />
         </Form-item>
-        <Form-item label="备注" prop="remark">
-          <Input type="textarea" :rows="3" v-model="formValidate.remark" placeholder="请输入备注"></Input>
+        <Form-item
+          label="备注"
+          prop="remark">
+          <Input
+            type="textarea"
+            :rows="3"
+            v-model="formValidate.remark"
+            placeholder="请输入备注" />
         </Form-item>
         <Form-item>
-          <Button type="primary" @click="handleSave" class="margin-right-sm">保存</Button>
-          <Button type="ghost" @click="$router.push(`${routePrefix}/cars/index`)">返回</Button>
+          <Button
+            type="primary"
+            @click="handleSave"
+            class="margin-right-sm">
+            保存
+          </Button>
+          <Button
+            type="ghost"
+            @click="$router.push(`${routePrefix}/cars/index`)">
+            返回
+          </Button>
         </Form-item>
       </Form>
     </div>
@@ -48,7 +94,6 @@
   import Uploader from '@/components/Uploader'
 
   export default {
-    name: 'form',
     async created () {
       this.routePrefix = helpers.getRoutePrefix(this.$route.params)
 
@@ -66,16 +111,7 @@
         routePrefix: '',
         alias: '',
         id: '',
-        formValidate: {
-          num: '',
-          picture: '',
-          km: '',
-          mot_time: '',
-          insurance_time: '',
-          buy_time: '',
-          owner: '',
-          remark: ''
-        },
+        formValidate: {},
         ruleValidate: {
           num: [
             {
@@ -97,6 +133,9 @@
       }
     },
     methods: {
+      handleDatePickerChange (key, value) {
+        this.formValidate[key] = value
+      },
       getDetails (id) {
         return this.$store.dispatch('getCar', { id })
       },
@@ -131,7 +170,7 @@
       'cars.car': {
         handler (newVal) {
           const { id, ...others } = newVal
-          this.formValidate = others
+          this.$set(this, 'formValidate', others)
         }
       }
     }
