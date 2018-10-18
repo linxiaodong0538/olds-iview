59b21a6c7af77b9958f3b36f152c66959a3ab4cd
zhaojintian <729234283@qq.com>
2018-10-19 00:46:45 +0800

    update

--- src/mixins/routeParams.js
+++ src/mixins/routeParams.js
@@ -2,11 +2,13 @@ export default {
   async beforeRouteUpdate (to, from, next) {
     this.routePrefix = this.$helpers.getRoutePrefix(to.params)
     this.alias = to.params.alias
+    this.id = to.params.id
 
     next()
   },
   async created () {
     this.routePrefix = this.$helpers.getRoutePrefix(this.$route.params)
     this.alias = this.$route.params.alias
+    this.id = this.$route.params.id
   }
 }
