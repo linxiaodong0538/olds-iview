439fd04ab2dbbba6f9a98256c098a8ab6121535f
zhaojintian <729234283@qq.com>
2018-11-06 16:43:10 +0800

    update

--- src/mixins/list.js
+++ src/mixins/list.js
@@ -7,10 +7,12 @@ export default {
   },
   async beforeRouteUpdate (to, from, next) {
     const { query } = to
+
     this.listPageCurrent = +query.listPageCurrent || 1
     this.listSearchWhere = query.listSearchWhere
       ? JSON.parse(query.listSearchWhere)
       : null
+
     next()
   },
   async created () {
@@ -20,5 +22,26 @@ export default {
     this.listSearchWhere = query.listSearchWhere
       ? JSON.parse(query.listSearchWhere)
       : null
+  },
+  methods: {
+    initSearchWhere (initWhere) {
+      this.cList.cSearch.where = this.listSearchWhere || this.$helpers.deepCopy(initWhere)
+    },
+    async resetSearch (initWhere) {
+      this.$router.push({
+        query: {
+          listPageCurrent: 1,
+          listSearchWhere: JSON.stringify(initWhere)
+        }
+      })
+    },
+    search () {
+      this.$router.push({
+        query: {
+          listPageCurrent: 1,
+          listSearchWhere: JSON.stringify(this.cList.cSearch.where)
+        }
+      })
+    }
   }
 }
