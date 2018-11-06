2cb5f0104a3ad4461e5c35f2eb0c17ac9e568e03
zhaojintian <729234283@qq.com>
2018-11-06 17:02:16 +0800

    update

--- src/mixins/form.js
+++ src/mixins/form.js
@@ -3,6 +3,9 @@ export default {
     resetFields (data = {}) {
       this.$refs.formValidate.resetFields()
       this.$set(this.cForm, 'formValidate', data)
+    },
+    initFields (data = {}) {
+      this.$set(this.cForm, 'formValidate', this.$helpers.deepCopy(data))
     }
   }
 }
